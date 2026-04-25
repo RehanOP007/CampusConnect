0e21e5ed830c70371d63ca086c9d0dd7966dad00
kavindu <kawmih538@gmail.com>
2026-04-25 13:08:55 +0530

    docs: Add documentation to Batch Rep Requests component

--- campusconnect/frontend/src/Component4/pages/Batchreprequests.jsx
+++ campusconnect/frontend/src/Component4/pages/Batchreprequests.jsx
@@ -1,3 +1,4 @@
+// Component 4: Batch Rep Requests - Manages pending batch representative approvals
 import React, { useState } from 'react';
 import { useTheme } from '../../contexts/ThemeContext';
 import { colors } from '../../contexts/ColorContext';
