a1a3fd5505354da5e7812a024705d1ba84c9b1f9
kavindu <kawmih538@gmail.com>
2026-04-25 13:03:14 +0530

    docs: Add documentation to Batch Statistics component

--- campusconnect/frontend/src/Component4/pages/Batchstats.jsx
+++ campusconnect/frontend/src/Component4/pages/Batchstats.jsx
@@ -1,3 +1,4 @@
+// Component 4: Batch Statistics - Displays student count and rep distribution
 import React from 'react';
 import { useTheme } from '../../contexts/ThemeContext';
 import { colors } from '../../contexts/ColorContext';
